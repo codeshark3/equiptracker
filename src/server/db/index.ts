d2c8852b64bda23544ad6c4532833358c84243fa
awentemi <jobaff03@gmail.com>
2024-12-16 23:25:15 +0000

    fixed vercel settings

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -17,31 +17,31 @@
 
 // export const db = drizzle(conn, { schema });
 // ************** local *******************
-import pg from "pg";
-// import { drizzle } from "drizzle-orm/vercel-postgres";
-import { drizzle } from "drizzle-orm/node-postgres";
-// import { sql } from "@vercel/postgres";
+// import pg from "pg";
+// // import { drizzle } from "drizzle-orm/vercel-postgres";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// // import { sql } from "@vercel/postgres";
 
-import * as schema from "./schema";
+// import * as schema from "./schema";
 
-const pool = new pg.Pool({
-  connectionString: process.env.POSTGRES_URL!,
-});
-//const db = drizzle(pool);
+// const pool = new pg.Pool({
+//   connectionString: process.env.POSTGRES_URL!,
+// });
+// //const db = drizzle(pool);
 
-// Use this object to send drizzle queries to your DBlogger: true casing: "snake_case" { schema }
-export const db = drizzle(pool, { schema });
+// // Use this object to send drizzle queries to your DBlogger: true casing: "snake_case" { schema }
+// export const db = drizzle(pool, { schema });
 
 // ************** vercel *******************
-// import { drizzle } from "drizzle-orm/vercel-postgres";
-// import { sql } from "@vercel/postgres";
-// import * as schema from "./schema";
-// import {
-//   pgTable,
-//   serial,
-//   text,
-//   timestamp,
-//   uniqueIndex,
-// } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import * as schema from "./schema";
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
 
-// export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema });
